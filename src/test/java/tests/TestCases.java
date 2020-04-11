81346995cacf1185b555f05ab20d05d0c3d4baac
LV Prasad Kancharla <lvpkanch@publicisgroupe.net>
2020-04-11 20:50:40 +0530

    method chaining in next line

--- src/test/java/tests/TestCases.java
+++ src/test/java/tests/TestCases.java
@@ -3,13 +3,14 @@ package tests;
 import org.testng.annotations.Test;
 
 import actions.ReusableActions;
+import pages.RegisterPage2;
 import pages.RegisterPage3;
 
 public class TestCases {
 	
 	ReusableActions registerAction=new ReusableActions();
 	//RegisterPage registerPage=new RegisterPage(ReusableActions.driver());
-	//RegisterPage2 regiPage2=new RegisterPage2(ReusableActions.driver());
+	RegisterPage2 regiPage2=new RegisterPage2(ReusableActions.driver());
 	RegisterPage3 regiPage3=new RegisterPage3(ReusableActions.driver());
 	
 //	 @Test
@@ -21,12 +22,14 @@ public class TestCases {
 //		 registerAction.teardown();
 //	 }
 	
-//	@Test
-//	public void designtestcaseusingMethodChaining(){
-//		regiPage2.inputFirstName("Prasad").inputLastName("Kancharla").inputEmail("test@mail.com");
-//		
-//		registerAction.teardown();
-//	}
+	@Test
+	public void designtestcaseusingMethodChaining(){
+		regiPage2.inputFirstName("Prasad")
+				 .inputLastName("Kancharla")
+				 .inputEmail("test@mail.com");
+		
+		registerAction.teardown();
+	}
 	
 	
 	@Test
